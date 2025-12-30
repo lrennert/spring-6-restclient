00682994ada156fd5b55f8163384adbc7e56d2ba
Lydia Rennert <lydia.rennert@gmail.com>
2025-12-30 20:33:48 +0100

    lre: Add updateBeer

--- src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
@@ -49,7 +49,15 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public BeerDTO updateBeer(BeerDTO beerDto) {
-        return null;
+        RestClient restClient = restClientBuilder.build();
+
+        restClient.put()
+                .uri(GET_BEER_BY_ID_PATH, beerDto.getId())
+                .body(beerDto)
+                .retrieve()
+                .toBodilessEntity();
+
+        return getBeerById(beerDto.getId());
     }
 
     @Override
